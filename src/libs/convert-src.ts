3b0ae7726f858bb2831ce0dc6b56c922500d4352
abtswath <zyapfine@gmail.com>
2022-07-28 21:02:15 +0800

    fix: 修复顶级 await 导致构建失败的问题

--- src/libs/convert-src.ts
+++ src/libs/convert-src.ts
@@ -1,12 +1,11 @@
 import { appDir, join } from '@tauri-apps/api/path';
 import { convertFileSrc } from '@tauri-apps/api/tauri';
 
-const dir = await appDir();
-
 export default async (file: string) => {
     if (file === '') {
         return '';
     }
+    const dir = await appDir();
     const path = await join(dir, 'images', file);
     return convertFileSrc(path);
 }
